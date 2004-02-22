900df81af48d59d099abf2a78a49afd17e8b05cd
Lorenzo Alberton <quipo@php.net>
2004-02-22 00:10:06 +0000

    add test cases
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@152035 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/MDB2_native_testcase.php
@@ -0,0 +1,151 @@
+<?php
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1998-2004 Lukas Smith, Lorenzo Alberton                |
+// | All rights reserved.                                                 |
+// +----------------------------------------------------------------------+
+// | MDB2 is a merge of PEAR DB and Metabases that provides a unified DB  |
+// | API as well as database abstraction for PHP applications.            |
+// | This LICENSE is in the BSD license style.                            |
+// |                                                                      |
+// | Redistribution and use in source and binary forms, with or without   |
+// | modification, are permitted provided that the following conditions   |
+// | are met:                                                             |
+// |                                                                      |
+// | Redistributions of source code must retain the above copyright       |
+// | notice, this list of conditions and the following disclaimer.        |
+// |                                                                      |
+// | Redistributions in binary form must reproduce the above copyright    |
+// | notice, this list of conditions and the following disclaimer in the  |
+// | documentation and/or other materials provided with the distribution. |
+// |                                                                      |
+// | Neither the name of Manuel Lemos, Tomas V.V.Cox, Stig. S. Bakken,    |
+// | Lukas Smith nor the names of his contributors may be used to endorse |
+// | or promote products derived from this software without specific prior|
+// | written permission.                                                  |
+// |                                                                      |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
+// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE      |
+// | REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,          |
+// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
+// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS|
+// |  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  |
+// | AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT          |
+// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY|
+// | WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          |
+// | POSSIBILITY OF SUCH DAMAGE.                                          |
+// +----------------------------------------------------------------------+
+// | Author: Lorenzo Alberton <l dot alberton at quipo dot it>            |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+class MDB2_Native_TestCase extends PHPUnit_TestCase
+{
+    //contains the dsn of the database we are testing
+    var $dsn;
+    //contains the options that should be used during testing
+    var $options;
+    //contains the name of the database we are testing
+    var $database;
+    //contains the MDB2 object of the db once we have connected
+    var $db;
+    // contains field names from the test table
+    var $fields;
+    // contains the types of the fields from the test table
+    var $types;
+
+    function MDB2_Native_Test($name) {
+        $this->PHPUnit_TestCase($name);
+    }
+
+    function setUp() {
+        $this->dsn = $GLOBALS['dsn'];
+        $this->options = $GLOBALS['options'];
+        $this->database = $GLOBALS['database'];
+        $this->db =& MDB2::connect($this->dsn, $this->options);
+        if (MDB2::isError($this->db)) {
+            $this->assertTrue(false, 'Could not connect to database in setUp - ' .$this->db->getMessage() . ' - ' .$this->db->getUserInfo());
+            exit;
+        }
+        $this->db->setDatabase($this->database);
+        $this->db->loadModule('Native');
+
+        $this->fields = array(
+                        'user_name',
+                        'user_password',
+                        'subscribed',
+                        'user_id',
+                        'quota',
+                        'weight',
+                        'access_date',
+                        'access_time',
+                        'approved'
+                        );
+
+        $this->types = array(
+                        'text',
+                        'text',
+                        'boolean',
+                        'integer',
+                        'decimal',
+                        'float',
+                        'date',
+                        'time',
+                        'timestamp'
+                    );
+    }
+
+    function tearDown() {
+        unset($this->dsn);
+        if (!MDB2::isError($this->db)) {
+            $this->db->disconnect();
+       }
+        unset($this->db);
+    }
+
+    function methodExists(&$class, $name) {
+        if (is_object($class)
+            && array_key_exists(strtolower($name), array_flip(get_class_methods($class)))
+        ) {
+            return true;
+        }
+        $this->assertTrue(false, 'method '. $name.' not implemented in '.get_class($class));
+        return false;
+    }
+
+    /**
+     * Test getInsertID()
+     */
+    function testGetInsertID()
+    {
+        if (!$this->methodExists($this->db->native, 'getInsertID')) {
+            return;
+        }
+        if (MDB2::isError($connect = $this->db->connect())) {
+            $this->assertTrue(false, 'Cannot connect to the db'.$connect->getMessage());
+        }
+
+        $this->assertTrue(false, 'TEST NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * Test deleteOID()
+     */
+    function testDeleteOID()
+    {
+        if (!$this->methodExists($this->db->native, 'deleteOID')) {
+            return;
+        }
+        if (MDB2::isError($connect = $this->db->connect())) {
+            $this->assertTrue(false, 'Cannot connect to the db'.$connect->getMessage());
+        }
+
+        $this->assertTrue(false, 'TEST NOT IMPLEMENTED YET');
+    }
+}
+
+?>
