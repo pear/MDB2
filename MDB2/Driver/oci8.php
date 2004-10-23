774201aeef0169e5c9629af408139be63220e227
Lorenzo Alberton <quipo@php.net>
2004-10-23 16:06:23 +0000

    typo
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@171164 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Driver/oci8.php
+++ MDB2/Driver/oci8.php
@@ -1027,7 +1027,7 @@ class MDB2_Statement_oci8 extends MDB2_Statement
         }
 
         $result_obj =& $this->db->_wrapResult($result, $ismanip, $this->types,
-            $result_class, $result_wrap_class, $this->row_offset, $this->row_imit);
+            $result_class, $result_wrap_class, $this->row_offset, $this->row_limit);
         return $result_obj;
     }
 
