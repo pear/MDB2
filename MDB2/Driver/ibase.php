459e11cbac7f86fbf6e8f26ed655adc4ac3c1c20
Lorenzo Alberton <quipo@php.net>
2004-10-23 10:31:08 +0000

    ibase_timefmt() doesn't exist in php5
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@171140 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Driver/ibase.php
+++ MDB2/Driver/ibase.php
@@ -341,8 +341,13 @@ class MDB2_Driver_ibase extends MDB2_Driver_Common
         $function = ($persistent ? 'ibase_pconnect' : 'ibase_connect');
         $connection = @call_user_func_array($function, $params);
         if ($connection > 0) {
-            @ibase_timefmt("%Y-%m-%d %H:%M:%S", IBASE_TIMESTAMP);
-            @ibase_timefmt("%Y-%m-%d", IBASE_DATE);
+            if (function_exists('ibase_timefmt')) {
+                @ibase_timefmt("%Y-%m-%d %H:%M:%S", IBASE_TIMESTAMP);
+                @ibase_timefmt("%Y-%m-%d", IBASE_DATE);
+            } else {
+                @ini_set("ibase.timestampformat", "%Y-%m-%d %H:%M:%S");
+                @ini_set("ibase.dateformat", "%Y-%m-%d");
+            }
             return $connection;
         }
 
