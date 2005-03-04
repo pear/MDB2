b1a1915180e464a73ee2a08f2e0ccc49a7b84298
Lukas Smith <lsmith@php.net>
2005-03-04 10:58:33 +0000

    - expect all errors during deleting
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@181311 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/MDB2_manager_testcase.php
+++ tests/MDB2_manager_testcase.php
@@ -107,8 +107,7 @@ class MDB2_Manager_TestCase extends PHPUnit_TestCase {
         if (!$this->methodExists($this->manager->db->manager, 'dropDatabase')) {
             return;
         }
-        $this->manager->db->expectError(array(MDB2_ERROR_CANNOT_DROP,
-                                              MDB2_ERROR_NOSUCHTABLE));
+        $this->manager->db->expectError('*');
         $result = $this->manager->db->manager->dropDatabase($this->database);
         $this->manager->db->popExpect();
         if (!MDB2::isError($result) || $result->getCode() != MDB2_ERROR_UNSUPPORTED) {
