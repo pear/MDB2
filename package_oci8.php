3f352c3a1c8f00ff242e8f2f197385b857ff033b
Lukas Smith <lsmith@php.net>
2005-08-14 07:23:15 +0000

    - updated changelog
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@193370 c90b9560-bf6c-de11-be94-00142212c4b1

--- package_oci8.php
+++ package_oci8.php
@@ -2,10 +2,8 @@
 
 require_once 'PEAR/PackageFileManager.php';
 
-$version = '0.1.1';
+$version = '0.1.2';
 $notes = <<<EOT
-- increased php dependency to 4.3.0 due to the usage of the streams API since beta5
-- fixed typo in _getTextDeclaration()
 - alterTable (Manager) now needs the full definition to work (use getTableFieldDefinition
  from Reverse module if you do not have a definition at hand) this eliminates the need
  of the declaration part in the alterTable array.
