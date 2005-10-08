d460821be58becf6202b8798135c72376dfd345a
David Coallier <davidc@php.net>
2005-10-08 20:04:29 +0000

    test config file
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@197934 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/config.php
@@ -0,0 +1,18 @@
+<?php
+// {{{ error reporting
+error_reporting(E_ERROR);
+// }}}
+// {{{ DSN Constants
+/**
+ * Constants used in Courrier_DB
+ *
+ * @see Courrier_DB::connect
+ */
+define ('DSN_PHPTYPE',  'mysql');
+define ('DSN_USERNAME', 'username');
+define ('DSN_PASSWORD', 'password');
+define ('DSN_HOSTNAME', 'hostname');
+define ('DSN_DATABASE', 'databasename');
+// }}}
+
+?>
