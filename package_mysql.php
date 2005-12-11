6b6f74218ca3c402061e98bb0e7e078480a5f7c0
Lukas Smith <lsmith@php.net>
2005-12-11 18:23:42 +0000

    - updated MDB2 dep
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@202665 c90b9560-bf6c-de11-be94-00142212c4b1

--- package_mysql.php
+++ package_mysql.php
@@ -73,7 +73,7 @@ $package->addMaintainer('lsmith', 'lead', 'Lukas Kahwe Smith', 'smith@pooteeweet
 
 $package->addDependency('php', '4.3.0', 'ge', 'php', false);
 $package->addDependency('PEAR', '1.0b1', 'ge', 'pkg', false);
-$package->addDependency('MDB2', '2.0.0beta7', 'ge', 'pkg', false);
+$package->addDependency('MDB2', '2.0.0RC1', 'ge', 'pkg', false);
 $package->addDependency('mysql', null, 'has', 'ext', false);
 
 $package->addglobalreplacement('package-info', '@package_version@', 'version');
