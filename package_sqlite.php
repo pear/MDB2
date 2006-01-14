5c0515813e25c5616565d3057b3eaf6d4965f36b
Lukas Smith <lsmith@php.net>
2006-01-14 17:08:07 +0000

    - bump version number and update changelog
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@205266 c90b9560-bf6c-de11-be94-00142212c4b1

--- package_sqlite.php
+++ package_sqlite.php
@@ -2,17 +2,9 @@
 
 require_once 'PEAR/PackageFileManager.php';
 
-$version = '0.2.3';
+$version = '0.2.4';
 $notes = <<<EOT
-- explicitly pass if the module is phptype specific in all loadModule calls (bug #6226)
-- fixed handling of autoincrement and unsigned in Reverse and Datatype module
-- tweaked handling of the native error message
-- typo fixes in error handling of nextResult() and numRows() calls
-- _fixIndexName() now just attempts to remove possible formatting
-- renamed _isSequenceName() to _fixSequenceName()
-- _fixSequenceName() now just attempts to remove possible formatting, and only
-  returns a boolean if no formatting was applied when the new "check" parameter is set to true
-- use MDB2_ERROR_ALREADY_EXISTS if the database to create already exists (bug #6481)
+- datatype 'clob' missing in mapNativeDatatype() (bug #6490)
 
 open todo items:
 - a number of the manager test cases fail because sqlite does not support adding
