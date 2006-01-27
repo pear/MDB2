3bdb35ceeefbeed3464386d5fccd335f65063d54
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2006-01-27 20:44:00 +0000

    tinyint(1) instead of plain tinyint for booleans
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@206021 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Driver/Datatype/mysql.php
+++ MDB2/Driver/Datatype/mysql.php
@@ -135,7 +135,7 @@ class MDB2_Driver_Datatype_mysql extends MDB2_Driver_Datatype_Common
             }
             return 'INT';
         case 'boolean':
-            return 'TINYINT';
+            return 'TINYINT(1)';
         case 'date':
             return 'DATE';
         case 'time':
