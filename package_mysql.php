7773ba3105c20856962ddfb2d572b48de6cdb71b
Lukas Smith <lsmith@php.net>
2006-02-05 15:54:52 +0000

    - bumped required version to RC5
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@206478 c90b9560-bf6c-de11-be94-00142212c4b1

--- package_mysql.php
+++ package_mysql.php
@@ -51,7 +51,7 @@ $package->addMaintainer('lsmith', 'lead', 'Lukas Kahwe Smith', 'smith@pooteeweet
 
 $package->addDependency('php', '4.3.0', 'ge', 'php', false);
 $package->addDependency('PEAR', '1.0b1', 'ge', 'pkg', false);
-$package->addDependency('MDB2', '2.0.0RC4', 'ge', 'pkg', false);
+$package->addDependency('MDB2', '2.0.0RC5', 'ge', 'pkg', false);
 $package->addDependency('mysql', null, 'has', 'ext', false);
 
 $package->addglobalreplacement('package-info', '@package_version@', 'version');
--- package_mysqli.php
+++ package_mysqli.php
@@ -58,7 +58,7 @@ $package->addMaintainer('lsmith', 'lead', 'Lukas Kahwe Smith', 'smith@pooteeweet
 
 $package->addDependency('php', '5.0.0', 'ge', 'php', false);
 $package->addDependency('PEAR', '1.0b1', 'ge', 'pkg', false);
-$package->addDependency('MDB2', '2.0.0RC4', 'ge', 'pkg', false);
+$package->addDependency('MDB2', '2.0.0RC5', 'ge', 'pkg', false);
 $package->addDependency('mysqli', null, 'has', 'ext', false);
 
 $package->addglobalreplacement('package-info', '@package_version@', 'version');
