33e894e4563ce3ac36c8e89bbe1472ff3a0ed940
Lorenzo Alberton <quipo@php.net>
2006-04-11 16:04:22 +0000

    filter duplicates
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@211204 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Driver/Manager/ibase.php
+++ MDB2/Driver/Manager/ibase.php
@@ -588,7 +588,7 @@ class MDB2_Driver_Manager_ibase extends MDB2_Driver_Manager_Common
             return $db;
         }
 
-        $result = $db->queryCol('SELECT RDB$VIEW_NAME FROM RDB$VIEW_RELATIONS');
+        $result = $db->queryCol('SELECT DISTINCT RDB$VIEW_NAME FROM RDB$VIEW_RELATIONS');
         if (PEAR::isError($result)) {
             return $result;
         }
