8f41067828725660287c8122f54c272049f9fa04
Lukas Smith <lsmith@php.net>
2006-08-19 15:21:41 +0000

    - check if safe_mode is enabled in fileExists() to determine what algo to use (Bug #8296)
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@218494 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2.php
+++ MDB2.php
@@ -911,10 +911,20 @@ class MDB2
      */
     function fileExists($file)
     {
-        $fp = @fopen($file, 'r', true);
-        if (is_resource($fp)) {
-            @fclose($fp);
-            return true;
+        // safe_mode does notwork with is_readable()
+        if (ini_get('safe_mode')) {
+            $fp = @fopen($file, 'r', true);
+            if (is_resource($fp)) {
+                @fclose($fp);
+                return true;
+            }
+        } else {
+             $dirs = explode(PATH_SEPARATOR, ini_get('include_path'));
+             foreach ($dirs as $dir) {
+                 if (is_readable($dir . DIRECTORY_SEPARATOR . $file)) {
+                     return true;
+                 }
+            }
         }
         return false;
     }
