2c36c6b18f4bd521f012a353f7ee1e86851156fb
David Coallier <davidc@php.net>
2006-10-09 14:21:25 +0000

    Coding Standards, sorry for previous commit
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@221432 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Extended.php
+++ MDB2/Extended.php
@@ -136,8 +136,8 @@ class MDB2_Extended extends MDB2_Module_Common
             if (is_array($result_types)) {
                 $keys = array_keys($result_types);
             } elseif (!empty($fields_values)) {
-				$keys = $fields_values;
-			} else {
+              $keys = $fields_values;
+            } else {
                 $keys = array();
             }
         } else {
