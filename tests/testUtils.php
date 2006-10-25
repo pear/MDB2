22c6581f33024553a2656369db5fb783fafc4e21
Lukas Smith <lsmith@php.net>
2006-10-25 12:13:29 +0000

    - better deal with non scalar arguments as 5.2.0 explodes otherwise
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@222352 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/testUtils.php
+++ tests/testUtils.php
@@ -34,7 +34,7 @@ function testCheck($testcase, $testmethod)
 /**
  * Little helper function that gets a backtrace if available
  */
-function getBacktrace()
+function getBacktrace($errline = 0)
 {
     $message = '';
     if (!function_exists('debug_backtrace')) {
@@ -59,7 +59,11 @@ function getBacktrace()
         $message.= $trace_item['function'];
 
         if (!empty($trace_item['args']) && is_array($trace_item['args'])) {
-            $message.= '('.@implode(', ', $trace_item['args']).')';
+            $args = array();
+            foreach ($trace_item['args'] as $arg) {
+              $args[] = is_scalar($arg) ? $arg : (is_object($arg) ? get_class($arg) : gettype($arg));
+            }
+            $message.= '('.implode(', ', $args).')';
         } else {
             $message.= '()';
         }
@@ -107,7 +111,7 @@ function errorHandler($errno, $errstr, $errfile, $errline)
         break;
     }
 
-    $message.= getBacktrace();
+    $message.= getBacktrace($errline);
 
     print_r($message);
 }
