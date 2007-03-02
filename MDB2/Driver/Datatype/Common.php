6947e907fa64a7199dc1d88a6d4b4cd5b0047ca4
Lorenzo Alberton <quipo@php.net>
2007-03-02 19:43:34 +0000

    pointless concatenation
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@231042 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Driver/Datatype/Common.php
+++ MDB2/Driver/Datatype/Common.php
@@ -1695,7 +1695,7 @@ class MDB2_Driver_Datatype_Common extends MDB2_Module_Common
                         'case insensitive LIKE matching requires passing the field name', __FUNCTION__);
                 }
                 $db->loadModule('Function', null, true);
-                $match = $db->function->lower($field).' '.'LIKE ';
+                $match = $db->function->lower($field).' LIKE ';
                 break;
             // case sensitive
             case 'LIKE':
