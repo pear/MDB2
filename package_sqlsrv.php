8bcc544d507cdf008b3567cda221fcc4b4ebb92a
Lorenzo Alberton <quipo@php.net>
2009-12-26 13:56:32 +0000

    new sqlsrv driver (request #16901) by Chris Pucci [chrisdpucci]
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@292641 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package_sqlsrv.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'PEAR/PackageFileManager2.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$version = 'XXX';
+$state = 'beta';
+$notes = <<<EOT
+
+EOT;
+
+$description = 'This is the SQL Server MDB2 driver.';
+$packagefile = './package_sqlsrv.xml';
+
+$options = array(
+    'filelistgenerator' => 'cvs',
+    'changelogoldtonew' => false,
+    'simpleoutput'      => true,
+    'baseinstalldir'    => '/',
+    'packagedirectory'  => './',
+    'packagefile'       => $packagefile,
+    'clearcontents'     => false,
+    'include'           => array('*sqlsrv*'),
+    'ignore'            => array('package_sqlsrv.php'),
+);
+
+$package = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
+$package->setPackageType('php');
+
+$package->clearDeps();
+$package->setPhpDep('4.3.0');
+$package->setPearInstallerDep('1.4.0b1');
+$package->addPackageDepWithChannel('required', 'MDB2', 'pear.php.net', 'XXX');
+$package->addExtensionDep('required', 'sqlsrv');
+
+$package->addRelease();
+$package->generateContents();
+$package->setReleaseVersion($version);
+$package->setAPIVersion($version);
+$package->setReleaseStability($state);
+$package->setAPIStability($state);
+$package->setNotes($notes);
+$package->setDescription($description);
+$package->addGlobalReplacement('package-info', '@package_version@', 'version');
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+    $package->writePackageFile();
+} else {
+    $package->debugPackageFile();
+}
