86f279775cd7e88e549311072ef30d44a2060ec9
Lorenzo Alberton <quipo@php.net>
2010-06-17 21:54:16 +0000

    revert latest commit, that broke LOB quoting
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@300551 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Driver/Datatype/Common.php
+++ MDB2/Driver/Datatype/Common.php
@@ -1245,17 +1245,15 @@ class MDB2_Driver_Datatype_Common extends MDB2_Module_Common
      *       a DBMS specific format.
      * @access protected
      */
-    function _readFile($value, $allow_url_include = false)
+    function _readFile($value)
     {
         $close = false;
         if (preg_match('/^(\w+:\/\/)(.*)$/', $value, $match)) {
             $close = true;
             if (strtolower($match[1]) == 'file://') {
                 $value = $match[2];
-                $value = @fopen($value, 'r');
-            } elseif ($allow_url_include) {
-                $value = @fopen($value, 'r');
             }
+            $value = @fopen($value, 'r');
         }
 
         if (is_resource($value)) {
@@ -1297,12 +1295,12 @@ class MDB2_Driver_Datatype_Common extends MDB2_Module_Common
         if (PEAR::isError($db)) {
             return $db;
         }
-
-        $value = $this->_readFile($value, $db->options['lob_allow_url_include']);
-        if (PEAR::isError($value)) {
-            return $value;
+        if ($db->options['lob_allow_url_include']) {
+            $value = $this->_readFile($value);
+            if (PEAR::isError($value)) {
+                return $value;
+            }
         }
-
         return $this->_quoteText($value, $quote, $escape_wildcards);
     }
 
