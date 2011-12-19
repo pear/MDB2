e838e2c522582d59cf64c0226041d86747ccdf0d
Daniel Convissor <danielc@php.net>
2011-12-19 18:22:21 +0000

    Docblock for import schema.
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@321200 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/import.schema.php
+++ tests/import.schema.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * A means to create the database and tables used by the test suite.
+ *
+ * The test suite builds the requisite structures automatically.  This script
+ * is made available in the event you want to do so manually.
+ *
+ * @package MDB2
+ * @category Database
+ * @author Daniel Convissor <danielc@php.net>
+ */
+
 error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
 require_once __DIR__ . '/autoload.inc';
 mdb2_test_db_object_provider();
