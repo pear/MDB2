920503c34d1edaa6dd171914a0ae5c70b20297e6
Daniel Convissor <danielc@php.net>
2011-12-19 18:24:25 +0000

    Oh, yeah, need to put in the incantations for people calling the all tests file directly via php.
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2/trunk@321221 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -11,6 +11,17 @@
  * @author Daniel Convissor <danielc@php.net>
  */
 
+// Keep tests from running twice when calling this file directly via PHPUnit.
+$call_main = false;
+if (strpos($_SERVER['argv'][0], 'phpunit') === false) {
+    // Called via php, not PHPUnit.  Pass the request to PHPUnit.
+    if (!defined('PHPUnit_MAIN_METHOD')) {
+        /** An indicator of which test was called. */
+        define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+        $call_main = true;
+    }
+}
+
 /**
  * Establish the test suite's environment.
  */
@@ -27,6 +38,10 @@ require_once __DIR__ . '/autoload.inc';
  * @author Daniel Convissor <danielc@php.net>
  */
 class AllTests {
+    public static function main() {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
 	public static function suite() {
 		$suite = new PHPUnit_Framework_TestSuite('MDB2 Unit Tests');
 
@@ -36,4 +51,8 @@ class AllTests {
 
 		return $suite;
 	}
+}
+
+if ($call_main) {
+    AllTests::main();
 }
